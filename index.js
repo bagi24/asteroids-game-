2503c4368f9eed528171e1869c66921eabaa8e6b
bagi24 <bagrat.injgia24@gmail.com>
2025-01-17 22:55:08 +0400

    added Spawning asteroids fun

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ class Player {
     c.rotate(this.rotation);
     c.translate(-this.position.x, -this.position.y);
 
+    c.beginPath();
     c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
     c.fillStyle = 'red';
     c.fill();
+    c.closePath();
 
     c.beginPath();
 
@@ -63,6 +65,28 @@ class Projectile {
   }
 }
 
+class Asteroid {
+  constructor({ position, velocity, radius }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = radius;
+  }
+  draw() {
+    c.beginPath();
+
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.strokeStyle = 'white';
+    c.stroke();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   velocity: { x: 0, y: 0 },
@@ -82,6 +106,55 @@ const FRICTION = 0.95;
 const PROJECTILE_SPEED = 3;
 
 const projectiles = [];
+const asteroids = [];
+
+window.setInterval(() => {
+  const index = Math.floor(Math.random() * 4);
+  let x, y;
+  let vx, vy;
+  let radius = 50 * Math.random() + 10;
+  switch (index) {
+    case 0: //left side of the screen
+      x = 0 - radius;
+      y = Math.random() * canvas.height;
+      vx = 1;
+      vy = 0;
+      break;
+    case 1: //bottom side of the screen
+      x = Math.random() * canvas.width;
+      y = canvas.height + radius;
+      vx = 0;
+      vy = -1;
+      break;
+    case 2: //right side of the screen
+      x = canvas.width + radius;
+      y = Math.random() * canvas.height;
+      vx = -1;
+      vy = 0;
+      break;
+    case 3: //top side of the screen
+      x = Math.random() * canvas.width;
+      y = 0 - radius;
+      vx = 0;
+      vy = 1;
+      break;
+  }
+
+  asteroids.push(
+    new Asteroid({
+      position: {
+        x: x,
+        y: y,
+      },
+      velocity: {
+        x: vx,
+        y: vy,
+      },
+      radius,
+    })
+  );
+  console.log(asteroids);
+}, 2000);
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -101,6 +174,22 @@ function animate() {
       projectile.position.y - projectile.radius > canvas.height
     ) {
       projectiles.splice(i, 1);
+    }
+  }
+
+  //asteroid menegment
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+    const asteroid = asteroids[i];
+    asteroid.update();
+
+    //garbage collection for asteroids
+    if (
+      asteroid.position.x + asteroid.radius < 0 ||
+      asteroid.position.x - asteroid.radius > canvas.width ||
+      asteroid.position.y + asteroid.radius < 0 ||
+      asteroid.position.y - asteroid.radius > canvas.height
+    ) {
+      asteroids.splice(i, 1);
     }
   }
 
