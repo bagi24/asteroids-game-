4ad3d859adbd9e998c6bffb8646842cc887f7da9
bagi24 <bagrat.injgia24@gmail.com>
2025-01-17 23:40:21 +0400

    added Game over fun

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Player {
-  constructor({ position, velocity, rotation }) {
-    this.position = position; //{x,y}
+  constructor({ position, velocity }) {
+    this.position = position; // {x, y}
     this.velocity = velocity;
     this.rotation = 0;
   }
@@ -24,15 +24,16 @@ class Player {
     c.fill();
     c.closePath();
 
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x, this.position.y, 100, 100)
     c.beginPath();
-
     c.moveTo(this.position.x + 30, this.position.y);
     c.lineTo(this.position.x - 10, this.position.y - 10);
     c.lineTo(this.position.x - 10, this.position.y + 10);
     c.closePath();
+
     c.strokeStyle = 'white';
     c.stroke();
-
     c.restore();
   }
 
@@ -41,17 +42,37 @@ class Player {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
   }
+
+  getVertices() {
+    const cos = Math.cos(this.rotation);
+    const sin = Math.sin(this.rotation);
+
+    return [
+      {
+        x: this.position.x + cos * 30 - sin * 0,
+        y: this.position.y + sin * 30 + cos * 0,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * 10,
+        y: this.position.y + sin * -10 + cos * 10,
+      },
+      {
+        x: this.position.x + cos * -10 - sin * -10,
+        y: this.position.y + sin * -10 + cos * -10,
+      },
+    ];
+  }
 }
 
 class Projectile {
-  constructor({ position, velocity, radius }) {
+  constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
     this.radius = 5;
   }
+
   draw() {
     c.beginPath();
-
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
     c.closePath();
     c.fillStyle = 'white';
@@ -71,9 +92,9 @@ class Asteroid {
     this.velocity = velocity;
     this.radius = radius;
   }
+
   draw() {
     c.beginPath();
-
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
     c.closePath();
     c.strokeStyle = 'white';
@@ -92,47 +113,52 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
-player.draw();
-
 const keys = {
-  w: { pressed: false },
-  a: { pressed: false },
-  d: { pressed: false },
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
 };
 
 const SPEED = 3;
 const ROTATIONAL_SPEED = 0.05;
-const FRICTION = 0.95;
+const FRICTION = 0.97;
 const PROJECTILE_SPEED = 3;
 
 const projectiles = [];
 const asteroids = [];
 
-window.setInterval(() => {
+const intervalId = window.setInterval(() => {
   const index = Math.floor(Math.random() * 4);
   let x, y;
   let vx, vy;
   let radius = 50 * Math.random() + 10;
+
   switch (index) {
-    case 0: //left side of the screen
+    case 0: // left side of the screen
       x = 0 - radius;
       y = Math.random() * canvas.height;
       vx = 1;
       vy = 0;
       break;
-    case 1: //bottom side of the screen
+    case 1: // bottom side of the screen
       x = Math.random() * canvas.width;
       y = canvas.height + radius;
       vx = 0;
       vy = -1;
       break;
-    case 2: //right side of the screen
+    case 2: // right side of the screen
       x = canvas.width + radius;
       y = Math.random() * canvas.height;
       vx = -1;
       vy = 0;
       break;
-    case 3: //top side of the screen
+    case 3: // top side of the screen
       x = Math.random() * canvas.width;
       y = 0 - radius;
       vx = 0;
@@ -153,62 +179,116 @@ window.setInterval(() => {
       radius,
     })
   );
+
   console.log(asteroids);
-}, 2000);
+}, 3000);
 
 function circleCollision(circle1, circle2) {
   const xDifference = circle2.position.x - circle1.position.x;
   const yDifference = circle2.position.y - circle1.position.y;
+
   const distance = Math.sqrt(xDifference * xDifference + yDifference * yDifference);
+
   if (distance <= circle1.radius + circle2.radius) {
-    console.log('two have collided');
     return true;
   }
+
   return false;
 }
 
+function circleTriangleCollision(circle, triangle) {
+  // Check if the circle is colliding with any of the triangle's edges
+  for (let i = 0; i < 3; i++) {
+    let start = triangle[i];
+    let end = triangle[(i + 1) % 3];
+
+    let dx = end.x - start.x;
+    let dy = end.y - start.y;
+    let length = Math.sqrt(dx * dx + dy * dy);
+
+    let dot =
+      ((circle.position.x - start.x) * dx + (circle.position.y - start.y) * dy) /
+      Math.pow(length, 2);
+
+    let closestX = start.x + dot * dx;
+    let closestY = start.y + dot * dy;
+
+    if (!isPointOnLineSegment(closestX, closestY, start, end)) {
+      closestX = closestX < start.x ? start.x : end.x;
+      closestY = closestY < start.y ? start.y : end.y;
+    }
+
+    dx = closestX - circle.position.x;
+    dy = closestY - circle.position.y;
+
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance <= circle.radius) {
+      return true;
+    }
+  }
+
+  // No collision
+  return false;
+}
+
+function isPointOnLineSegment(x, y, start, end) {
+  return (
+    x >= Math.min(start.x, end.x) &&
+    x <= Math.max(start.x, end.x) &&
+    y >= Math.min(start.y, end.y) &&
+    y <= Math.max(start.y, end.y)
+  );
+}
+
 function animate() {
-  window.requestAnimationFrame(animate);
+  const animationId = window.requestAnimationFrame(animate);
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
+
   player.update();
 
   for (let i = projectiles.length - 1; i >= 0; i--) {
     const projectile = projectiles[i];
     projectile.update();
 
-    //garbage collection for projectiles
+    // garbage collection for projectiles
     if (
       projectile.position.x + projectile.radius < 0 ||
       projectile.position.x - projectile.radius > canvas.width ||
-      projectile.position.y + projectile.radius < 0 ||
-      projectile.position.y - projectile.radius > canvas.height
+      projectile.position.y - projectile.radius > canvas.height ||
+      projectile.position.y + projectile.radius < 0
     ) {
       projectiles.splice(i, 1);
     }
   }
 
-  //asteroid menegment
+  // asteroid management
   for (let i = asteroids.length - 1; i >= 0; i--) {
     const asteroid = asteroids[i];
     asteroid.update();
 
-    //garbage collection for asteroids
+    if (circleTriangleCollision(asteroid, player.getVertices())) {
+      console.log('GAME OVER');
+      window.cancelAnimationFrame(animationId);
+      clearInterval(intervalId);
+    }
+
+    // garbage collection for projectiles
     if (
       asteroid.position.x + asteroid.radius < 0 ||
       asteroid.position.x - asteroid.radius > canvas.width ||
-      asteroid.position.y + asteroid.radius < 0 ||
-      asteroid.position.y - asteroid.radius > canvas.height
+      asteroid.position.y - asteroid.radius > canvas.height ||
+      asteroid.position.y + asteroid.radius < 0
     ) {
       asteroids.splice(i, 1);
     }
 
-    //projectiles
+    // projectiles
     for (let j = projectiles.length - 1; j >= 0; j--) {
       const projectile = projectiles[j];
 
-      if (circleCollision(projectile, asteroid)) {
-        console.log('Big Success');
+      if (circleCollision(asteroid, projectile)) {
         asteroids.splice(i, 1);
         projectiles.splice(j, 1);
       }
@@ -222,6 +302,7 @@ function animate() {
     player.velocity.x *= FRICTION;
     player.velocity.y *= FRICTION;
   }
+
   if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED;
   else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED;
 }
@@ -231,7 +312,6 @@ animate();
 window.addEventListener('keydown', event => {
   switch (event.code) {
     case 'KeyW':
-      console.log('W');
       keys.w.pressed = true;
       break;
     case 'KeyA':
@@ -253,16 +333,14 @@ window.addEventListener('keydown', event => {
           },
         })
       );
-      console.log(projectiles);
+
       break;
   }
-  player.draw();
 });
 
 window.addEventListener('keyup', event => {
   switch (event.code) {
     case 'KeyW':
-      console.log('W');
       keys.w.pressed = false;
       break;
     case 'KeyA':
@@ -272,5 +350,4 @@ window.addEventListener('keyup', event => {
       keys.d.pressed = false;
       break;
   }
-  player.draw();
 });
